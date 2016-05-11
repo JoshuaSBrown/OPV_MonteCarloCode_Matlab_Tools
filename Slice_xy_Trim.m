279c79279fadf47bf805fd4c38784a2c11edc1a1
JoshuaSBrown <joshbro42867@yahoo.com>
2016-05-11 16:18:17 -0600

    Added functions to ouput energies in log scale to better appreciate differences

--- Slice_xy_Trim.m
+++ Slice_xy_Trim.m
@@ -1,6 +1,7 @@
-function Slice_xy( FileName, z )
+function Slice_xy_Trim( FileName, z, Einv, Limit )
 %This function takes the input of an .xyz file and grabs a plane at z
-
+%Einv is the energy we creating a log scale around. 
+%Limit creates bounds out of which everything else is excluded. 
 ext = FileName(end-3:end);
 core = FileName(1:end-4);
 if(strcmp(ext,'.xyz')~=1)
@@ -12,7 +13,7 @@ if(z<0)
 end
 
 fid = fopen(FileName);
-FileName2 = strcat(core,'Slice_xy_z',num2str(z),'.xyz');
+FileName2 = strcat(core,'Slice_xy_z_Trim',num2str(z),'.xyz');
 fid2 = fopen(FileName2,'w');
 fprintf(fid2,'              \n\n');
 TotalLines = str2double(fgetl(fid));
@@ -38,8 +39,24 @@ D3 = D(D(:,3)~=0,3);
 D4 = D(D(:,3)~=0,4);
 D5 = D(D(:,3)~=0,5);
 
+Excess = (D4<=(Einv-Limit))+(D4>=(Einv+Limit));
+Einv = Excess.*D4;
+
+D1 = Excess.*D1;
+D2 = Excess.*D2;
+D3 = Excess.*D3;
+D4 = Excess.*D4;
+D5 = Excess.*D5;
+
+D1 = D1(D1~=0);
+D2 = D2(D2~=0);
+D3 = D3(D3~=0);
+D4 = D4(D4~=0);
+D5 = D5(D5~=0);
+Einv = Einv(Einv~=0);
+
 totalines = length(D1);
-fprintf(fid2,'C %f %f %f %f %f\n', [D1 D2 D3 D4 D5]');
+fprintf(fid2,'C %f %f %f %f %f %f\n', [D1 D2 D3 D4 D5 Einv]');
 
 fclose(fid);
 
--- Slice_xy_logEnergies.m
+++ Slice_xy_logEnergies.m
@@ -1,6 +1,6 @@
-function Slice_xy( FileName, z )
+function Slice_xy_logEnergies( FileName, z, Einv )
 %This function takes the input of an .xyz file and grabs a plane at z
-
+%Einv is the energy we creating a log scale around. 
 ext = FileName(end-3:end);
 core = FileName(1:end-4);
 if(strcmp(ext,'.xyz')~=1)
@@ -12,7 +12,7 @@ if(z<0)
 end
 
 fid = fopen(FileName);
-FileName2 = strcat(core,'Slice_xy_z',num2str(z),'.xyz');
+FileName2 = strcat(core,'Slice_xy_z_Elog',num2str(z),'.xyz');
 fid2 = fopen(FileName2,'w');
 fprintf(fid2,'              \n\n');
 TotalLines = str2double(fgetl(fid));
@@ -38,8 +38,10 @@ D3 = D(D(:,3)~=0,3);
 D4 = D(D(:,3)~=0,4);
 D5 = D(D(:,3)~=0,5);
 
+Einv = log(abs(D4-Einv)).*sign(Einv-D4);
+
 totalines = length(D1);
-fprintf(fid2,'C %f %f %f %f %f\n', [D1 D2 D3 D4 D5]');
+fprintf(fid2,'C %f %f %f %f %f %f\n', [D1 D2 D3 D4 D5 Einv]');
 
 fclose(fid);
 
